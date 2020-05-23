810ebcd53e7e8c1cc7891d459c151fa35a95d661
obergad <46499070+obergad@users.noreply.github.com>
2020-05-22 22:43:52 -0700

    added images to homejs

--- react-bootstrap/src/Home.js
+++ react-bootstrap/src/Home.js
@@ -9,9 +9,9 @@ export const Home = () => (
 </div>
 <div>
   <h3> Competitive Beaver Esport Teams!</h3>
-  <img src="./public/csgo-logo-png.png" alt="CSGo">
-  <img src="./public/1200px-Overwatch_League_logo.svg.png" alt="Overwatch">
-  <img src="./public/kisspng-tom-clancy-s-rainbow-six-siege-tom-clancy-s-rainbo-rainbow-six-5b27cb8f233f14.3860990915293346711444.jpg" alt="Rainbow 6 Siege">
-  <img src="./public/lol-logo-rendered-hi-res.png" alt="League of Legends">
+  <img src="./public/csgo-logo-png.png" alt="CSGo" id ='game-img'>
+  <img src="./public/1200px-Overwatch_League_logo.svg.png" alt="Overwatch" id ='game-img'> 
+  <img src="./public/kisspng-tom-clancy-s-rainbow-six-siege-tom-clancy-s-rainbo-rainbow-six-5b27cb8f233f14.3860990915293346711444.jpg" alt="Rainbow 6 Siege" id ='game-img'>
+  <img src="./public/lol-logo-rendered-hi-res.png" alt="League of Legends" id ='game-img'>
  </div> 
 )
