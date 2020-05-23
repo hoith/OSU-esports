f39e7145046d1ae9ae5493391eabce4d43f68a99
obergad <46499070+obergad@users.noreply.github.com>
2020-05-22 22:42:36 -0700

    added images to home.js

--- react-bootstrap/src/Home.js
+++ react-bootstrap/src/Home.js
@@ -2,9 +2,16 @@
 import React from 'react';
 
 export const Home = () => (
-<div>
+<div class = "title-text">
 <h2>OSU Esports </h2>
 <p>LETS GO BEAVERS!</p>
 <p>ITS IN THE GAME! </p>
 </div>
+<div>
+  <h3> Competitive Beaver Esport Teams!</h3>
+  <img src="./public/csgo-logo-png.png" alt="CSGo">
+  <img src="./public/1200px-Overwatch_League_logo.svg.png" alt="Overwatch">
+  <img src="./public/kisspng-tom-clancy-s-rainbow-six-siege-tom-clancy-s-rainbo-rainbow-six-5b27cb8f233f14.3860990915293346711444.jpg" alt="Rainbow 6 Siege">
+  <img src="./public/lol-logo-rendered-hi-res.png" alt="League of Legends">
+ </div> 
 )
