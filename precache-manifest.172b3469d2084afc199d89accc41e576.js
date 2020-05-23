4581bb9e9bf1d9d704545b09a97fab7ca49bedb0
MiguelCoding <miguelruizjax@gmail.com>
2020-05-22 21:17:45 -0700

    Updates

---
+++ precache-manifest.172b3469d2084afc199d89accc41e576.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6229f5cd714c997bf37121ad4260ed8b",
+    "url": "/OSU-esports/index.html"
+  },
+  {
+    "revision": "b97242d0bbbb10a3ed8d",
+    "url": "/OSU-esports/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "994b7992035c639d6f68",
+    "url": "/OSU-esports/static/js/2.4fa0d8b0.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/OSU-esports/static/js/2.4fa0d8b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b97242d0bbbb10a3ed8d",
+    "url": "/OSU-esports/static/js/main.b485a304.chunk.js"
+  },
+  {
+    "revision": "c8884ffb3612905af6a1",
+    "url": "/OSU-esports/static/js/runtime-main.7fe89880.js"
+  }
+]);
