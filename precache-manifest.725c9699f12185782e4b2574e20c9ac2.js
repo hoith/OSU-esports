a61c382b2b9849fee219e0d62f69d277cf7f396a
Ghaith Shan <shang@oregonstate.edu>
2020-05-22 22:03:02 -0700

    Updates

---
+++ precache-manifest.725c9699f12185782e4b2574e20c9ac2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06e499ef277105465c0d1e9fc2e044ff",
+    "url": "/OSU-esports/index.html"
+  },
+  {
+    "revision": "0a52a822653bd3df4ebb",
+    "url": "/OSU-esports/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "3c209c29189f726964e4",
+    "url": "/OSU-esports/static/js/2.db9826ab.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/OSU-esports/static/js/2.db9826ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a52a822653bd3df4ebb",
+    "url": "/OSU-esports/static/js/main.62fff07e.chunk.js"
+  },
+  {
+    "revision": "c8884ffb3612905af6a1",
+    "url": "/OSU-esports/static/js/runtime-main.7fe89880.js"
+  }
+]);
